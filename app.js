44c66f93d8990953f4b1cf578b69924c43ff65ba
Miguel <mabo544@gmail.com>
2022-09-15 14:56:40 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Servidor para aplicación de notas
+
+// Requerir librerias necesarias.
+const express = require("express");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+// const https = require("https");
+
+//Inicializacion de Servidor
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Ruta raiz
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/index.html");
+})
+
+//Configuracion de puerto escucha del Servidor
+app.listen(3000, ()=>{
+  console.log("Servidor escuchando en el puerto de pruebas 3000.".bgWhite.blue);
+});
